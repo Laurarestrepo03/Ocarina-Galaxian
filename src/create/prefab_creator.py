5c59293f6645bf2707ba82236baa3a8974b91291
laura <l.restrepop@uniandes.edu.co>
2024-04-29 21:45:06 -0500

    Add: create player

--- src/create/prefab_creator.py
+++ src/create/prefab_creator.py
@@ -5,6 +5,8 @@ import pygame
 from src.ecs.components.c_surface import CSurface
 from src.ecs.components.c_transform import CTransform
 from src.ecs.components.c_velocity import CVelocity
+from src.ecs.components.tags.c_tag_player import CTagPlayer
+from src.engine.service_locator import ServiceLocator
 
 def create_square(ecs_world:esper.World, size:pygame.Vector2,
                     pos:pygame.Vector2, vel:pygame.Vector2, col:pygame.Color):
@@ -26,3 +28,15 @@ def create_sprite(ecs_world:esper.World, pos:pygame.Vector2, vel:pygame.Vector2,
     ecs_world.add_component(sprite_entity,
                             CSurface.from_surface(surface))
     return sprite_entity
+
+def create_player(ecs_world:esper.World, player_info:dict) -> int:
+    player_surface = ServiceLocator.images_service.get(player_info["image"])
+    size = player_surface.get_rect().size
+    pos = pygame.Vector2(player_info["spawn_point"]["x"] - (size[0]/2),
+                         player_info["spawn_point"]["y"] - (size[1]/2))
+    vel = pygame.Vector2(0,0)
+    player_entity = create_sprite(ecs_world, pos, vel, player_surface)
+    ecs_world.add_component(player_entity, CTagPlayer())
+    #ecs_world.add_component(player_entity, CAnimation(player_info["animations"]))
+    #ecs_world.add_component(player_entity, CPlayerState())
+    return player_entity
