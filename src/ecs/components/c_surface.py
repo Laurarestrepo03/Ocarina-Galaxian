b54f06e9b296f8b6de1634dd1c0eb5a026350ae1
laura <l.restrepop@uniandes.edu.co>
2024-04-29 21:08:03 -0500

    Add: complete surface component

--- src/ecs/components/c_surface.py
+++ src/ecs/components/c_surface.py
@@ -4,3 +4,23 @@ class CSurface:
     def __init__(self, size:pygame.Vector2, color:pygame.Color) -> None:
         self.surf = pygame.Surface(size)
         self.surf.fill(color)
+        self.area = self.surf.get_rect()
+
+    @classmethod
+    def from_surface(cls, surface:pygame.Surface):
+        c_surf = cls(pygame.Vector2(0,0), pygame.Color(0,0,0))
+        c_surf.surf = surface
+        c_surf.area = surface.get_rect()
+        return c_surf
+    
+    @classmethod
+    def from_text(cls, text: str, font:pygame.font, color:pygame.Color):
+        c_surf = cls(pygame.Vector2(0,0), pygame.Color(0,0,0))
+        c_surf.surf = font.render(text, False, color)
+        c_surf.area = c_surf.surf.get_rect()
+        return c_surf
+    
+    def get_area_relative(area:pygame.Rect, pos_topLeft:pygame.Vector2):
+        new_rect = area.copy()
+        new_rect.topleft = pos_topLeft.copy()
+        return new_rect
