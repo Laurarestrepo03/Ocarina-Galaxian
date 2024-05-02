56a4cfdb919c93ab71a6cd107e9944224ddf4702
laura <l.restrepop@uniandes.edu.co>
2024-05-01 20:45:20 -0500

    Fix: player limit

--- src/ecs/systems/s_player_limit.py
+++ src/ecs/systems/s_player_limit.py
@@ -12,15 +12,8 @@ def system_player_limit(ecs_world:esper.World, screen:pygame.Surface):
     components = ecs_world.get_components(CTransform, CVelocity, CSurface, CTagPlayer)
 
     for _, (c_t, _, c_s, _) in components:
-        #TODO: limitar no con pantalla sino un poco menos
         player_rect = CSurface.get_area_relative(c_s.area, c_t.pos)
-        if player_rect.left < 0:
-            c_t.pos.x = 0 
-        elif player_rect.right > screen_rect.width:
-            c_t.pos.x = screen_rect.width - player_rect.width 
-
-        if player_rect.top < 0:
-            c_t.pos.y = 0
-        elif player_rect.bottom > screen_rect.height:
-            c_t.pos.y = screen_rect.height - player_rect.height 
-
+        if player_rect.left < 26:
+            c_t.pos.x = 26
+        elif player_rect.right > screen_rect.width - 26:
+            c_t.pos.x = screen_rect.width - player_rect.width - 26
