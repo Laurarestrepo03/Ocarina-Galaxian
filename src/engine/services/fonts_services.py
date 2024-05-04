a4ac3f4604d872eba6b3095b2223b878cb897daa
lacortesz <la.cortesz1@uniandes.edu.co>
2024-05-04 15:20:25 -0500

    Ajuste fonts_services.py

--- src/engine/services/fonts_services.py
+++ src/engine/services/fonts_services.py
@@ -3,10 +3,11 @@ import pygame
 class FontsService:
     def __init__(self) -> None:
         self._fonts = {}
-        pass
     
     def get(self, path:str, size:int) -> pygame.font:
-        if path not in self._fonts or size != self._fonts:
-            self._fonts[path] = pygame.font.Font(path, size)
-        return self._fonts[path]
+        font_key = path + "-" + str(size)
+        if path not in self._fonts:     
+            self._fonts[font_key] = pygame.font.Font(path, size)
+        return self._fonts[font_key]
+    
     
