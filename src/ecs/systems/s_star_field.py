e2e4fbbedd70501f180175e14fc16f15cc563212
Leonardo Barrios Poveda <o.barriosp@uniandes.edu.co>
2024-05-10 00:19:41 -0500

    Se ajusta la creación y parpadeo de las estrellas eliminando CStarfield y añadiendo CBlink y system_blink

--- src/ecs/systems/s_star_field.py
+++ src/ecs/systems/s_star_field.py
@@ -6,10 +6,9 @@ from src.ecs.components.c_velocity import CVelocity
 
 
 def system_star_field(ecs_world:esper.World, window_cfg, delta_time):
-    star_entities = ecs_world.get_component(CSurface)
-    for entity, c_surface in star_entities:
+    star_entities = ecs_world.get_components(CTransform, CVelocity)
+    for entity, (c_transform, c_velocity) in star_entities:
         c_transform = ecs_world.component_for_entity(entity, CTransform)
-        c_velocity = ecs_world.component_for_entity(entity, CVelocity)
         c_transform.pos += c_velocity.vel * delta_time
         
         if c_transform.pos.y > window_cfg["size"]["h"]:
